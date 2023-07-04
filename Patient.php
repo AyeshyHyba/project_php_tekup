9df6e18bbc442be6296db8ae5de98df574a973cc
Hyba222 <ayechihiba2016@gmail.com>
2023-07-04 16:36:35 +0100

    update pages

--- Patient.php
+++ Patient.php
@@ -17,20 +17,25 @@ class Patient {
         $pwd1 = $data['pwd1'];
         $pwd2 = $data['pwd2'];
         if($pwd1!=$pwd2){
-            echo "password does not match";
+            echo `<script>
+            window.location.href= "Register.php";
+            alert("password does not match");
+            </script>`;
         }
         else {
             $verif = $this->db->query("select * from Patient where nomP='$nom' and prP='$pr'");
             if ($verif->fetch() ) {
-                echo "Patient already exists";
+                echo `<script>
+            window.location.href= "Register.php";
+            alert("Patient already exist");
+            </script>`;
             }else {
                 $query = "insert into Patient values ('','$nom','$pr','$mail','$pwd1')";
                 $r = $this->db->exec($query);
                 if($r){
-                    echo "user added succ";
+                    header("location:login.html");
                 }
-                else
-                {
+                else{
                     echo "pbe d'ajout";
                 }
             }
@@ -39,3 +44,4 @@ class Patient {
     }
 
 }
+
--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+include("Patient.php");
+$c = new Patient();
+if(isset($_POST['register'])){
+    $c->addPatient($_POST);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,76 +57,20 @@
       </div>
       <nav class="col-lg-9 col-6">
         <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a>
-        <ul id="top_access">
-          <li><a href="login.html"><i class="pe-7s-user"></i></a></li>
-          <li><a href="register-doctor.html"><i class="pe-7s-add-user"></i></a></li>
-        </ul>
         <div class="main-menu">
           <ul>
             <li class="submenu">
               <a href="#0" class="show-submenu">Home<i class="icon-down-open-mini"></i></a>
-              <ul>
-                <li><a href="index.html">Home Default</a></li>
-                <li><a href="index-8.html">KenBurns Slider</a></li>
-                <li><a href="index-2.html">Home Version 2</a></li>
-                <li><a href="index-3.html">Home Version 3</a></li>
-                <li><a href="index-4.html">Home Version 4</a></li>
-                <li><a href="index-7.html">Home with Map</a></li>
-                <li><a href="index-6.html">Revolution Slider</a></li>
-                <li><a href="index-5.html">With Cookie Bar (EU law)</a></li>
-              </ul>
             </li>
             <li class="submenu">
               <a href="#0" class="show-submenu">Pages<i class="icon-down-open-mini"></i></a>
               <ul>
-                <li class="third-level"><a href="#0">List pages</a>
-                  <ul>
-                    <li><a href="list.html">List page</a></li>
-                    <li><a href="grid-list.html">List grid page</a></li>
-                    <li><a href="list-map.html">List map page</a></li>
-                  </ul>
-                </li>
-                <li class="third-level"><a href="#0">Detail pages</a>
-                  <ul>
-                    <li><a href="detail-page.html">Detail page 1</a></li>
-                    <li><a href="detail-page-2.html">Detail page 2</a></li>
-                    <li><a href="detail-page-3.html">Detail page 3</a></li>
-                    <li><a href="detail-page-working-booking.html">Detail working booking</a></li>
-                  </ul>
-                </li>
-                <li><a href="submit-review.html">Submit Review</a></li>
-                <li><a href="blog-1.html">Blog</a></li>
-                <li><a href="badges.html">Badges</a></li>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="login-2.html">Login 2</a></li>
-                <li><a href="register-doctor.html">Register Doctor</a></li>
-                <li><a href="register-doctor-working.html">Working doctor register</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="contacts.html">Contacts</a></li>
+                <li><a href="RegisterDoctor.html">Register Doctor</a></li>
+                 <li><a href="Register.php">Register</a></li>
+                <li><a href="aboutUs.html">About Us</a></li>
+                <li><a href="contactUs.html">Contacts</a></li>
               </ul>
             </li>
-            <li class="submenu">
-              <a href="#0" class="show-submenu">Extra Elements<i class="icon-down-open-mini"></i></a>
-              <ul>
-                <li><a href="booking-page.html">Booking page</a></li>
-                <li><a href="confirm.html">Confirm page</a></li>
-                <li><a href="faq.html">Faq page</a></li>
-                <li><a href="coming_soon/index.html">Coming soon</a></li>
-                <li class="third-level"><a href="#0">Pricing tables</a>
-                  <ul>
-                    <li><a href="pricing-tables-3.html">Pricing tables 1</a></li>
-                    <li><a href="pricing-tables.html">Pricing tables 2</a></li>
-                    <li><a href="pricing-tables-2.html">Pricing tables 3</a></li>
-                  </ul>
-                </li>
-                <li><a href="icon-pack-1.html">Icon pack 1</a></li>
-                <li><a href="icon-pack-2.html">Icon pack 2</a></li>
-                <li><a href="icon-pack-3.html">Icon pack 3</a></li>
-                <li><a href="404.html">404 page</a></li>
-              </ul>
-            </li>
-            <li><a href="#0">Buy this template</a></li>
           </ul>
         </div>
         <!-- /main-menu -->
@@ -133,36 +84,28 @@
 <main>
   <div class="bg_color_2">
     <div class="container margin_60_35">
-      <div id="login-2">
+      <div id="login">
         <h1>Please login to Findoctor!</h1>
-        <form>
-          <div class="box_form clearfix">
-            <div class="box_login">
-              <a href="#0" class="social_bt facebook">Login with Facebook</a>
-              <a href="#0" class="social_bt google">Login with Google</a>
-              <a href="#0" class="social_bt linkedin">Login with Linkedin</a>
+        <div class="box_form">
+          <form>
+            <div class="form-group">
+              <input type="email" class="form-control"  name="mail"  placeholder="Your email address">
             </div>
-            <div class="box_login last">
-              <div class="form-group">
-                <input type="email" class="form-control" placeholder="Your email address">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control" placeholder="Your password" name="password" id="password">
-                <a href="#0" class="forgot"><small>Forgot password?</small></a>
-              </div>
-              <div class="form-group">
-                <input class="btn_1" type="submit" value="Login">
-              </div>
+            <div class="form-group">
+              <input type="password" class="form-control"  placeholder="Your password" name="pwd" id="password">
             </div>
-          </div>
-        </form>
+            <a href="#0"><small>Forgot password?</small></a>
+            <div class="form-group text-center add_top_20">
+              <input class="btn_1 medium" type="submit" value="Login">
+            </div>
+          </form>
+        </div>
         <p class="text-center link_bright">Do not have an account yet? <a href="#0"><strong>Register now!</strong></a></p>
       </div>
       <!-- /login -->
     </div>
   </div>
 </main>
-<!-- /main -->
 
 <footer>
   <div class="container margin_60_35">
